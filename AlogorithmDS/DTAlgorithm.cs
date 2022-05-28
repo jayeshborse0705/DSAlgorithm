8b066d7510e12c34a0df74624afb3e9b330d2e0e
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-05-28 22:01:02 +0530

    [jayesh] insertion sort

---
+++ AlogorithmDS/DTAlgorithm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlogorithmDS
+{
+    internal class DTAlgorithm
+    {
+        public void Insert()
+        {
+            string[] names = { "Rat", "Zebra", "Bull", "Monkey" };
+            InsertSort(names);
+            foreach (var list in names)
+            {
+                Console.WriteLine(list);
+            }
+        }
+        static void InsertSort(IComparable[] array)
+        {
+            int i, j;
+
+            for (i = 1; i < array.Length; i++)
+            {
+
+                IComparable value = array[i];
+                j = i - 1;
+                while ((j >= 0) && (array[j].CompareTo(value) > 0))
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = value;
+            }
+        }
+    }
+}
---
+++ AlogorithmDS/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace AlgorithmDS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DTAlgorithm algorithms = new DTAlgorithm();
+            algorithms.Insert();
+        }
+    }
+}
